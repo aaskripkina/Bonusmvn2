fe713593be5d14c3e568959217fc4222d9cef79a
Alexandra Skripkina <aa.skripkina@gmail.com>
2023-01-21 12:09:59 +0200

    BonusMilesService.java changed

--- src/main/java/BonusMilesService.java
+++ src/main/java/BonusMilesService.java
@@ -1,13 +1,13 @@
 public class BonusMilesService {
 
-        public long calculate(long amount, boolean registered) {
-            int percent = registered ? 3 : 1;
-            long bonus = amount * percent / 100;
-            long limit = 500;
-            if(bonus > limit) {
-                bonus = limit;
-            }
-            return bonus;
+    public long calculate(long amount, boolean registered) {
+        int percent = registered ? 3 : 1;
+        long bonus = amount * percent / 100;
+        long limit = 500;
+        if (bonus > limit) {
+            bonus = limit;
+        }
+        return bonus;
 
     }
 }
